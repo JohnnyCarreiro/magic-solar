61a43814052a325221db4a2ebb92a27bb34fb711
Johnny <jcarreiropublicidade@gmail.com>
2021-12-14 21:47:21 -0300

    Import and Pass data to respective component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
 import Head from 'next/head'
 import { Container } from '../styles/Home'
-import HeroSection from '@/components/HeroSection'
+import { HeroSection } from '@/components/HeroSection'
 import { InfoSection } from '@/components/InfoSection'
-import { homeObjOne, homeObjTwo, homeObjThree } from '@/components/InfoSection/Data'
 import { Services } from '@/components/Services'
+import { homeObjOne, homeObjTwo, homeObjThree, heroSection, services } from '@/Data/Data'
+
 
 export default function Home() {
+  console.log('Hoeme: ', services)
   return (
     <Container>
       <Head>
         <title>Magic Solar</title>
       </Head>
-      <HeroSection />
+      <HeroSection {...heroSection} />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
+      <Services services={services} />
       <InfoSection {...homeObjThree} />
-      <Services />
     </Container>
   )
 }
